b11618e60e43bff946e0e7bb7bd726b30c0420c0
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-05-05 14:56:31 +0200

    add imgSource variable

--- script/script.js
+++ script/script.js
@@ -2,6 +2,7 @@ const app = Vue.createApp({
     data() {
         return{
             team: 'Forza Milan!',
+            imgSource: 'assets/img/037060a022d928837048b3a884e2dad8.jpg'
         }
     }
 });
