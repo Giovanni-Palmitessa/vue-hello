94fba28a30a1f7c9dc8e36a1a67a4ef256bcb2b0
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-05-05 15:26:32 +0200

    add flex to container

--- script/script.js
+++ script/script.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
     data() {
         return{
             team: 'Forza Milan!',
-            imgSource: 'assets/img/037060a022d928837048b3a884e2dad8.jpg'
+            imgSource: 'assets/img/037060a022d928837048b3a884e2dad8.jpg',
+            displayFlex: 'd-flex',
         }
     }
 });
