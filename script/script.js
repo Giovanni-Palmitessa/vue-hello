7998f8e3ecbb08929d2ccc21dcb04ca6133d8309
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-05-05 15:55:01 +0200

    adjust key displayFlex

--- script/script.js
+++ script/script.js
@@ -3,8 +3,7 @@ const app = Vue.createApp({
         return{
             team: 'Forza Milan!',
             imgSource: 'assets/img/037060a022d928837048b3a884e2dad8.jpg',
-            displayFlex: 'd-flex',
-            justContent: 'justify-content-center',
+            displayFlex: 'd-flex justify-content-center',
         }
     }
 });
